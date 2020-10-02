b15eb8f5c8104802a5bc1166897cc013e0192726
VJ1224 <vanshjain1224@gmail.com>
2020-10-02 21:49:26 +0530

    Added user auth

---
+++ php/authenticate.php
@@ -0,0 +1,9 @@
+<?php
+session_start();
+ 
+if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+    echo "0";
+} else {
+    echo "1";
+}
+?>
---
+++ php/login.php
@@ -0,0 +1,32 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "cia2_project";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+session_start();
+
+$user = (isset($_POST['username']) ? $_POST['username']: '');
+$pass = strtoupper(hash('sha256', (isset($_POST['password']) ? $_POST['password']: '')));
+
+$stmt = $conn->prepare("SELECT username FROM users WHERE username=? AND password=?");
+
+$stmt->bind_param("ss", $user, $pass);
+$stmt->execute();
+$stmt->store_result();
+
+if($stmt->num_rows > 0) {
+    echo "1";
+    $_SESSION["loggedin"] = true;
+} else {
+    echo "0";
+    $_SESSION["loggedin"] = false;
+}
+
+$conn->close();
+?>
