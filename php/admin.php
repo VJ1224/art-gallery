1e6a523afc663b6d21a78de851237f1fd75df95e
VJ1224 <vanshjain1224@gmail.com>
2020-10-03 12:14:19 +0530

    Added admin interface to add art

---
+++ php/admin.php
@@ -0,0 +1,22 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "cia2_project";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$aname = (isset($_POST['aname']) ? $_POST['aname']: '');
+$artist = (isset($_POST['artist']) ? $_POST['artist']: '');
+$price = intval(isset($_POST['price']) ? $_POST['price']: '0');
+$atype = (isset($_POST['atype']) ? $_POST['atype']: '');
+
+$stmt = $conn->prepare("INSERT INTO artwork VALUES (?,?,?,?)");
+
+$stmt->bind_param("ssis", $aname, $artist, $price, $atype);
+$stmt->execute();
+$conn->close();
+?>
