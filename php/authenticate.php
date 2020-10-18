4fa74bce0d761b7b345393ab5b7a6e89d8fbd566
VJ1224 <vanshjain1224@gmail.com>
2020-10-18 19:51:21 +0530

    Sorted css

--- php/authenticate.php
+++ php/authenticate.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
- 
-if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
+
+if (!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true) {
     echo "0";
 } else {
     echo "1";
