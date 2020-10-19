08ec28484f2f785ed40fafc5e70f6ba0ba392226
VJ1224 <vanshjain1224@gmail.com>
2020-10-19 14:01:53 +0530

    Added blog post load

--- php/posts.php
+++ php/posts.php
@@ -7,8 +7,16 @@ if (is_dir($dir)) {
     echo "<ul class='list-group'>";
     
     foreach ($posts as $post) {
-        echo "<a href='#' class='list-group-item list-group-item-action'>" . 
-        substr($post, 0, (strrpos($post, "."))) . "</a>";
+        $title = substr($post, 0, (strrpos($post, ".")));
+        $content = file_get_contents('../blog/' . $post);
+        $summary = substr($content, strpos($content, "<p>"), strpos($content, "</p>")+4);
+        
+        echo "<a href='#' class='list-group-item list-group-item-action' onclick='loadPost(this)'>
+        <div class='d-flex w-100 justify-content-between'>
+        <h5 class='mb-1'>" . $title . "</h5>
+        </div>
+        <p class='mb-1'>" . $summary . "</p>
+        </a>";
     }
 
     echo "</ul>";
