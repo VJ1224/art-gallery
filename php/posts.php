26d7947e317284cd1b5b4714469c145d0294d3c5
VJ1224 <vanshjain1224@gmail.com>
2020-10-20 19:11:33 +0530

    Added post date

--- php/posts.php
+++ php/posts.php
@@ -14,10 +14,12 @@ if (is_dir($dir)) {
         $title = substr($post, 0, (strrpos($post, ".")));
         $content = file_get_contents('../blog/' . $post);
         $summary = substr($content, strpos($content, "<p>"), strpos($content, "</p>")+4);
-        
+        $date = date("d F Y", filemtime('../blog/' . $post));
+
         echo "<a href='#' class='list-group-item list-group-item-action' onclick='loadPost(this)'>
         <div class='d-flex w-100 justify-content-between'>
         <h5 class='mb-1'>" . $title . "</h5>
+        <small>" . $date ."</small>
         </div>
         <p class='mb-1'>" . $summary . "...</p>
         </a>";
